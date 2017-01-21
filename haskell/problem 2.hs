17b1d9da3ebda9f7bdd0839bf71c10d48678688f
Emil Kirilov <emil.kirilovk@gmail.com>
2017-01-22 00:26:40 +0200

    second problem's final solution

--- haskell/problem 2.hs
+++ haskell/problem 2.hs
@@ -8,11 +8,17 @@ prime' n k
   |n `rem` k == 0  = False
   |otherwise       = prime' n (k + 1)
 
-touplesToNumbers arr = map (\x -> (fst x) + 2 * (snd x) ) arr
+touplesToNumbers arr = map (\x -> (fst x) + 2 * (snd x)* (snd x) ) arr
 
+removeDuplicates [] = []
+removeDuplicates (x:xs) = [x] ++ removeDuplicates (filter (\el -> el /= x) xs)
 
-removeDuplicates (x:xs) 
-  |x elem xs = removeDuplicates xs
-  |otherwise = x:(removeDuplicates xs)
+--odd, complex, can be represented by the sum of a prime number and twice the square of a natural number
+notSpecial = removeDuplicates $ touplesToNumbers [(i, j) | i <- [2..99], prime i, 
+                                                           j <- [1..6], odd (i + 2 * j * j), 
+                                                           not (prime (i + 2 * j * j)), 100 > i + 2 * j * j, 9 < i + 2 * j * j]   
 
-   
+--odd, complex
+special = [ i | i <- [11,13..99], not (prime i)] 
+                                                           
+numberOfSpecials = length notSpecial - length notSpecial 
