03da64afe2dcba34a32128cd798568a1281b07f6
Emil Kirilov <emil.kirilovk@gmail.com>
2017-02-04 23:37:48 +0200

    adds a few functions

--- haskell/learnMeAHaskell/higherOrderFunction2.hs
+++ haskell/learnMeAHaskell/higherOrderFunction2.hs
@@ -16,7 +16,7 @@ chain n
     | otherwise  = n : chain (n * 3 + 1)
 
 numLongChains :: Int
-numLongChains = length (filter (\ xs -> length xs > 15) (map chain [1..100]))
+numLongChains = length $ filter (\ xs -> length xs > 15) $ map chain [1..100]
 --numLongChains = length (filter isLong (map chain [1..100]))
     --where isLong xs = length xs > 15
 
@@ -25,3 +25,19 @@ elem' y ys = foldr (\ x acc -> if x == y then True else acc) False ys
 
 maximum' :: (Ord a) => [a] -> a
 maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+--reverse' = foldl (\ acc x -> x : acc) []
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\ x acc -> if p x then x : acc else acc) []
+
+sqrtSums :: Int
+sqrtSums = (length . takeWhile (<1000) . scanl1 (+) $ map sqrt [1..]) + 1 --scanl1 will count the first 2 elements for one
+
+--ghci> map ($ 3) [(4+), (10*), (^2), sqrt]
+--[7.0,30.0,9.0,1.7320508075688772]
+
+negateAll :: (Num a) => [a] -> [a]
+negateAll list = map (negate . abs) list
